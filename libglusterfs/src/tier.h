972a8ae94068ec635a29808d9e233eaf83f81b51
Dan Lambright <dlambrig@redhat.com>
2014-07-29 16:14:06 -0400

    Add tier.h

---
+++ libglusterfs/src/tier.h
@@ -0,0 +1,24 @@
+/*
+  Copyright (c) 2008-2012 Red Hat, Inc. <http://www.redhat.com>
+  This file is part of GlusterFS.
+
+  This file is licensed to you under your choice of the GNU Lesser
+  General Public License, version 3 or any later version (LGPLv3 or
+  later), or the GNU General Public License, version 2 (GPLv2), in all
+  cases as published by the Free Software Foundation.
+*/
+
+#ifndef _TIER_H
+
+typedef enum {
+        GF_COMBINE = 1,
+        GF_SPLIT   = 2
+} tier_grouptype_t;
+
+struct _tier_group {
+        tier_grouptype_t type;
+};
+
+typedef struct _tier_group tier_group_t;
+
+#endif /* _TIER_H */
